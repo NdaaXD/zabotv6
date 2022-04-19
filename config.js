133d47ba46318ce35206b21b3689253186dbcc3b
NdaaXD <98279739+NdaaXD@users.noreply.github.com>
2022-04-19 07:26:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
-global.owner = [['6288220118685', 'Ndaa', true]] // Put your number here
+global.owner = [['6282145614361', 'Ndaa', true]] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
